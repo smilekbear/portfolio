44fb272115737c531537254e2fd8a328150dd197
Moo_96 <59558215+smilekbear@users.noreply.github.com>
2025-12-08 18:06:56 +0900

    25.12.08 Commit
    - Github Action Test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ const Page = () => {
                     </clipPath>
                 </defs>
             </svg>,
-            value : '김동현1199'
+            value : '김동현1122'
         },
         {
             icon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
